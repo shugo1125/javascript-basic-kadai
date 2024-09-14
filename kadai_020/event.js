d36c3c28507516c1dd8120f2716b26db3f0c2e0a
shugo1125 <iamsyu25@icloud.com>
2024-09-14 11:22:34 +0900

    2回目のコミット

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,11 +1,5 @@
 const Btn = document.getElementById('btn');
-
- Btn.addEventListener('click', () => {
-
- const text= document.getElementById('text');
- text.style.display='none'; 
- const parentElement = document.querySelector('body');
-   const text2 = document.createElement('h2');
-   text2.textContent = 'ボタンがクリックされました';ï
-   parentElement.insertBefore(text2);Ï
- });
+const text = document.getElementById('text');
+Btn.addEventListener('click', () => {
+  text.textContent = 'ボタンがクリックされました'; ï
+});
