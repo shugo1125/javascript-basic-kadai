020b9835443d25adad3e319fc8935956bf485331
shugo1125 <iamsyu25@icloud.com>
2024-09-18 20:06:05 +0900

    kadai_mdnを追加

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,9 @@
+const date = new Date();
+
+const year = date.getFullYear();
+const month = date.getMonth() + 1;
+const day = date.getDate();
+
+
+const formattedDate = `${year}年${month}月${day}日`;
+console.log(formattedDate);
